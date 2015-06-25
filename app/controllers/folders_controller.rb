bc0d562286372022f96b521cc53ee8c0cab7ae0c
Brit Butler <redline6561@gmail.com>
2015-06-25 12:06:43 -0400

    Add basic folders create method.

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,5 @@
 class FoldersController < ApplicationController
+  before_action :authenticate_with_token!
 
   def index
   end
@@ -7,6 +8,21 @@ class FoldersController < ApplicationController
   end
 
   def create
+    parent_id = params[:parent_id]
+    if parent_id
+      parent = current_user.curations.find(parent_id)
+    end
+
+    # @folder = Curation.create(name: params[:name],
+    #                           user: current_user)
+    @folder = current_user.curations.new(name: params[:name])
+    if @folder.save
+      @folder.move_to_child_of(parent) if parent_id && parent
+      render json: { folder: @folder }, status: :created
+    else
+      render json: { errors: @folder.errors.full_messages },
+        status: :unprocessable_entity
+    end
   end
 
   def delete
